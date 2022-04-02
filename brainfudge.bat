3c248d586974063f8abdcdf02fdf8f035e2658fd
shibby <shivank.chhaya@gmail.com>
2022-04-02 13:41:56 -0700

    Update brainfudge.bat

--- brainfudge.bat
+++ brainfudge.bat
@@ -1,12 +1,10 @@
-while getopts f:e: opt
-do
-  case $opt in
-  f)
-  opts=`more "$OPTARG"`
-  python bfr.py "$opts"
-  ;;
-  e)
-  python bfr.py "$OPTARG"
-  ;;
-  esac
-done
+@echo off
+if %1==-f (
+  setlocal EnableDelayedExpansion
+  set opts=
+  for /f "tokens=* delims=" %%x in (%2) do set opts=!opts!%%x
+  python bfr.py !opts!
+)
+if %1==-e (
+  python bfr.py %~2
+)
